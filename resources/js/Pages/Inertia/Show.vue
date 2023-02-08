e3786b807c30d264f58fdf19ccbfbb3e4b07fc74
AkinoriKita <aknor1004@gmail.com>
2023-02-08 22:32:37 +0900

    inerti-vue.js

---
+++ resources/js/Pages/Inertia/Show.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { router } from '@inertiajs/vue3'
+
+defineProps({
+    id : String,
+    blog: Object
+})
+const deleteComfirm = id => {
+    router.delete(`/inertia/${id}`, {
+        onBefore: () => confirm('本当に削除しますか？')
+    })
+}
+
+</script>
+
+<template>
+    {{ id }}<br>
+    {{ blog.title }}<br>
+    <button @click="deleteComfirm(blog.id)">Delete</button>
+</template>
---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import { ref } from "vue";
+
+const newTitle = ref('')
+const newContent = ref('')
+</script>
+
+<template>
+Inertiaテストです。<br>
+<a href="/">aタグ</a><br>
+<Link href="/">Link</Link><br>
+<Link :href="route('inertia.index')">namae tuki route</Link><br>
+<Link :href="route('inertia.show', { id: 1 })">route para test</Link>
+
+<div class="mb-8"></div>
+<input type="text" name="newTitle" v-model="newTitle"><br>
+<input type="text" name="newContent" v-model="newContent"><br>
+<Link as="button" method="POST" :href="route('inertia.store')"
+:data="{ 
+    title: newTitle,
+    content: newContent
+ }">DB STORE</Link>
+</template>
